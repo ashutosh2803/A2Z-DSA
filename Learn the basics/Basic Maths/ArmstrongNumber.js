c8325debd881898b9483a93e38edbe2091768b3d
Ashutosh Kumar <ashuisalive@gmail.com>
2026-01-13 03:10:46 +0530

    git commit --date="2026-01-12 14:30:00" -m "added basic maths"

---
+++ Learn the basics/Basic Maths/ArmstrongNumber.js
@@ -0,0 +1,18 @@
+function ArmstrongNumber(n) {
+  let digitCount = Math.floor(Math.log10(n) + 1);
+  let original = n;
+  let calculation = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    calculation = Math.pow(rem, digitCount) + calculation;
+    n = Math.floor(n / 10);
+  }
+  return original === calculation;
+}
+
+function main() {
+  let n = 151;
+  console.log(ArmstrongNumber(n));
+}
+
+main();
