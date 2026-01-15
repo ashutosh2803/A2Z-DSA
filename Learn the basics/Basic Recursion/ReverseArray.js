53743f94b3f846fd70a9729f1dc0e66cf4ca3fcb
Ashutosh Kumar <ashuisalive@gmail.com>
2026-01-16 04:54:55 +0530

    git commit --date="yesterday" -m "added basic recursion"

---
+++ Learn the basics/Basic Recursion/ReverseArray.js
@@ -0,0 +1,19 @@
+function ReverseArray(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+}
+
+function main() {
+  let arr = [1, 2, 3, 4, 5];
+  console.log(ReverseArray(arr));
+}
+
+main();
