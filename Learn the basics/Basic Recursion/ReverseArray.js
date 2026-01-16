030db2fe50cb9f70c098703887bd3590fe9a5e66
Ashutosh Kumar <ashuisalive@gmail.com>
2026-01-16 05:50:50 +0530

    added basic recursion

--- Learn the basics/Basic Recursion/ReverseArray.js
+++ Learn the basics/Basic Recursion/ReverseArray.js
@@ -1,4 +1,4 @@
-function ReverseArray(arr) {
+function ReverseArrayTwoPointer(arr) {
   let start = 0;
   let end = arr.length - 1;
   while (start < end) {
@@ -11,9 +11,24 @@ function ReverseArray(arr) {
   return arr;
 }
 
+function ReverseArrayUsingRecursion(i, arr) {
+  if (i >= arr.length / 2) return arr;
+
+  let temp = arr[i];
+  arr[i] = arr[arr.length - i - 1];
+  arr[arr.length - i - 1] = temp;
+
+  return ReverseArrayUsingRecursion(i + 1, arr);
+}
+
 function main() {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  console.log(ReverseArray(arr));
+  let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  console.log("Two Pointer: ", ReverseArrayTwoPointer(arr1));
+  console.log("Recursion: ", ReverseArrayUsingRecursion(0, arr2));
 }
 
 main();
+
+// Time Complexity = O(N)
+// Space Complexity = O(N) - recursion call stack
