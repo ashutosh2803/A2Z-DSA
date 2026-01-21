3f877c80efbc85b6b9d5c7e213c2aa86388cf47a
Ashutosh Kumar <ashuisalive@gmail.com>
2026-01-21 12:00:00 +0530

    added middle of LL

--- Learn LinkedList/Medium problems of LL/MiddleOfLL.js
+++ Learn LinkedList/Medium problems of LL/MiddleOfLL.js
@@ -14,6 +14,7 @@ class Node {
 // Brute Force Approach
 // Time Complexity: O(N) + O(N/2) => O(N)
 // Space Complexity: O(1)
+
 function findMiddle(head) {
     if (head === null || head.next === null) {
         return head;
